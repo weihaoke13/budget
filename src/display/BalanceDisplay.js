77afefa5c4b39a3f3107466a4e47f6f89d331c8b
Weihao Ke <weihaoke13@gmail.com>
2019-09-28 16:06:09 -0700

    first commit

---
+++ src/display/BalanceDisplay.js
@@ -0,0 +1,50 @@
+import React,{Component} from 'react';
+import {BudgetConsumer} from '../store';
+
+const BalanceDisplay =()=>{
+    return(
+        <BudgetConsumer>
+            {value =>{
+                const totalExpense = value.expenses.length > 0?(
+                    value.expenses.reduce((acc, curr)=>{
+                        acc+= parseInt(curr.amount)
+                        return acc
+                    },0)) : 0;
+
+                    return (
+            
+                        <div className='row'>
+                            <div className='col-lg-4'>
+                                <div className='card'>
+                                    <div className='card-header'>Budget</div>
+                                    <div className='card-body'>
+                                        <h5 className='text-center card-tilte'>{value.budget}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-lg-4'>
+                                <div className='card'>
+                                    <div className='card-header'>Expenses</div>
+                                    <div className='card-body'>
+                                        <h5 className='text-center card-tilte'>{totalExpense}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-lg-4'>
+                                <div className='card'>
+                                    <div className='card-header'>Balance</div>
+                                    <div className='card-body'>
+                                        <h5 className='text-center card-tilte'>{value.budget - totalExpense}</h5>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    )
+        }}
+        </BudgetConsumer>
+    )
+}
+
+export default BalanceDisplay;
---
+++ src/display/DisplayBudget.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BalanceDisplay from './BalanceDisplay';
+import ExpenseList from './ExpenseList';
+
+
+const DisplayBudget =() =>{
+    return (
+        <div className='card card-body'>
+            <h3 className = 'text-center'>Yout Budget Info</h3>
+            <BalanceDisplay />
+            <ExpenseList />
+        </div>
+    )
+}
+
+export default DisplayBudget;
