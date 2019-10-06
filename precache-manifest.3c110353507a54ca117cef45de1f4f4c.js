4707a3da48ba59260af425753861b6f64627eba1
Weihao Ke <weihaoke13@gmail.com>
2019-10-05 23:28:58 -0700

    Updates

---
+++ precache-manifest.3c110353507a54ca117cef45de1f4f4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662468ebcd7bd4f645011c73be423e75",
+    "url": "/budget/index.html"
+  },
+  {
+    "revision": "85c5e48798186a2d4cd5",
+    "url": "/budget/static/css/main.ca2f7b43.chunk.css"
+  },
+  {
+    "revision": "6e807413191dcfe74382",
+    "url": "/budget/static/js/2.ce33f056.chunk.js"
+  },
+  {
+    "revision": "85c5e48798186a2d4cd5",
+    "url": "/budget/static/js/main.85a7d4d8.chunk.js"
+  },
+  {
+    "revision": "ee41c54f5152e7e0f4ce",
+    "url": "/budget/static/js/runtime~main.54c8dce2.js"
+  }
+]);
